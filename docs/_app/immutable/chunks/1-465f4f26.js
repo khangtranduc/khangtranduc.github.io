8e6a7e158342258c2e7ba0be8bf2a6c59f106e57
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2022-12-01 21:02:27 +0700

    testing 3

---
+++ docs/_app/immutable/chunks/1-465f4f26.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-1cddc7ac.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-f4d7290d.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-deefd07d.js";export{t as component};
