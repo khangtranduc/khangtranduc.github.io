2e1f5010d808505b44f39c2307f19cc03593dc46
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2022-12-03 16:32:16 +0700

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<script>
-    import "chota/dist/chota.css";
-</script>
-
-
 <slot/>
-=======
->>>>>>> parent of b8a84bf (Update +layout.svelte)
-=======
->>>>>>> parent of b8a84bf (Update +layout.svelte)
