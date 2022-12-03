217bceb3926d3c1cacdd4afe4ee36bc59f0c4ae8
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2022-12-03 17:28:23 +0700

    Revert "Update +page.svelte"
    This reverts commit b616b02cc7dc937f1356a1eff87cb3a6e1337e6a.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- <script>
 	import { Alert } from 'flowbite-svelte';
 </script>
@@ -9,18 +8,3 @@
 	</Alert>
 </div> -->
 <h1>Hello World!</h1>
-=======
-<<<<<<< HEAD
-<script>
-    import {Button} from "svelte-chota";
-    let button_text = 1;
-</script>
-
-<button on:mouseenter={e => button_text = 2}
-        on:mouseleave={e => button_text = 3}>
-    Button Text {button_text}
-</button>
-=======
-<h1>Hello World!</h1> 
->>>>>>> parent of 7df396c (bruhmius)
->>>>>>> parent of 419a98a (reverting back)
