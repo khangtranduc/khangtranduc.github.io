419a98a982f191b54bd36f680f10e66d9a83bb0d
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2022-12-03 16:33:54 +0700

    reverting back

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-<script>
-    import {Button} from "svelte-chota";
-    let button_text = 1;
-</script>
-
-<button on:mouseenter={e => button_text = 2}
-        on:mouseleave={e => button_text = 3}>
-    Button Text {button_text}
-</button>
-=======
-<h1>Hello World!</h1> 
->>>>>>> parent of 7df396c (bruhmius)
+<h1>Hello World!</h1>
