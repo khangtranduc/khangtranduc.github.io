5336fc0b697b6960846ae73a0563912a02dc8440
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2022-12-03 16:31:10 +0700

    bruh

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,9 @@
-
+<<<<<<< HEAD
+<script>
+    import "chota/dist/chota.css";
+</script>
 
 
 <slot/>
+=======
+>>>>>>> parent of b8a84bf (Update +layout.svelte)
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,9 @@
-<h1>Hello World</h1>
+<script>
+    import {Button} from "svelte-chota";
+    let button_text = 1;
+</script>
+
+<button on:mouseenter={e => button_text = 2}
+        on:mouseleave={e => button_text = 3}>
+    Button Text {button_text}
+</button>
