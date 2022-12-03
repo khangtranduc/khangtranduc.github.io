2445e7779444931ecf7121e82dc82d0a406c8850
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2022-12-03 23:42:27 +0700

    update 1

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,54 @@
 <script>
   import "../app.postcss";
+  import { 
+    DarkMode,
+    Navbar, NavBrand, NavLi, NavUl, NavHamburger,
+    Footer, FooterIcon,
+    P,
+  } from 'flowbite-svelte';
+  import {
+    Github, Discord, Whatsapp, Telegram, Linkedin, Envelope
+  } from 'svelte-bootstrap-svg-icons';
 </script>
 
+<Navbar let:hidden let:toggle>
+    <NavBrand href="/">
+        <img src="/profile.jpg" class="mr-3 h-6 sm:h-9 rounded-full" alt="Wavy Logo"/>
+        <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+            Khang Tran
+        </span>
+    </NavBrand>
+    <DarkMode/>
+    <NavHamburger on:click={toggle} />
+    <NavUl {hidden}>
+      <NavLi href="/" active={true}>Home</NavLi>
+      <NavLi href="/about">About</NavLi>
+    </NavUl>
+  </Navbar>
+
 <slot />
+
+
+<Footer footerType="socialmedia" class="flex justify-around">
+    <P color="text-gray-400 dark:text-white"  size="xs">Tran Duc Khang</P>
+    <div class="flex flex-row-reverse flex-1 gap-x-5">
+        <FooterIcon href="/" class="text-gray-400 hover:text-black">
+        <Github/>
+        </FooterIcon>
+        <FooterIcon href="/" class="text-gray-400 hover:text-black">
+        <Discord/>
+        </FooterIcon>
+        <FooterIcon href="/" class="text-gray-400 hover:text-black">
+        <Whatsapp/>
+        </FooterIcon>
+        <FooterIcon href="/" class="text-gray-400 hover:text-black">
+        <Telegram/>
+        </FooterIcon>
+        <FooterIcon href="/" class="text-gray-400 hover:text-black">
+        <Linkedin/>
+        </FooterIcon>
+        <FooterIcon href="/" class="text-gray-400 hover:text-black">
+        <Envelope/>
+        </FooterIcon>
+    </div>
+</Footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,38 @@
 <script>
-	import { Alert } from 'flowbite-svelte';
+    import {
+        P,
+        FooterIcon,
+        Avatar,
+    } from "flowbite-svelte"
+    import{
+        Whatsapp, Envelope, Linkedin,
+    } from "svelte-bootstrap-svg-icons"
 </script>
 
-<div class="p-8">
-	<Alert>
-		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
-	</Alert>
+<div class="py-8 flex flex-row gap-x-12">
+    <section class="ml-20 flex flex-col items-center justify-center">
+        <Avatar src="/mugshot-square.jpeg" size="xl" border/>
+        <P size="sm" weight="semibold">Tran Duc Khang</P>
+        <P size="xs">Student at NUS High</P>
+        <div class="flex justify-center gap-x-3 py-3">
+            <FooterIcon href="/" class="text-gray-400 hover:text-black">
+                <Envelope/>
+            </FooterIcon>
+            <FooterIcon href="/" class="text-gray-400 hover:text-black">
+                <Whatsapp/>
+            </FooterIcon>
+            <FooterIcon href="/" class="text-gray-400 hover:text-black">
+                <Linkedin/>
+            </FooterIcon>
+        </div>
+    </section>
+    <!-- <article class="p-3 text-black dark:text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in 
+        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+        deserunt mollit anim id est laborum.
+    </article> -->
 </div>
