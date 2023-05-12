0692022b888546acd1453a3a2ec72eb4b0b73e9b
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2023-05-12 15:41:42 +0800

    temp-2

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,27 @@
-<script>
-
+<script lang="ts">
+    import { goto } from '$app/navigation';
+    import Darkmode from './darkmode.svelte';
 </script>
 
 <main>
-    <nav>
+    <nav class="container-fluid">
+        <ul on:click={() => goto('/')} on:keydown>
+          <li><img src="/profile.jpg" alt=""/></li>
+          <li><strong>Khang Tran</strong></li>
+        </ul>
         <ul>
-            <li><img src="/profile" alt=""/></li>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact" role="button">Contact</a></li>
+          <li><Darkmode /></li>
         </ul>
     </nav>
 </main>
 
+<style lang="scss">
+    img {
+        width: 2rem;
+        height: 2rem;
+        border-radius: 100%;
+    }
+</style>
