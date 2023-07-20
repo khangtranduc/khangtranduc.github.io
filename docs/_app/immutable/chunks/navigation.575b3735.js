d46252d02ef74ffc9cee82c0d19dda0ef664eff9
Tran Duc Khang <79127891+khangtranduc@users.noreply.github.com>
2023-07-20 17:09:02 +0800

    added some static files + changed splashes

--- docs/_app/immutable/chunks/navigation.575b3735.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./singletons.131c5cf1.js";var n=(o=>(o[o.Ongoing=0]="Ongoing",o[o.Completed=1]="Completed",o[o.Sidelined=2]="Sidelined",o))(n||{});const d=e("goto");export{n as S,d as g};
--- docs/_app/immutable/chunks/stores.d0cd1cd7.js
+++
@@ -1,1 +0,0 @@
-import"./index.f8dbf630.js";import{s as e}from"./singletons.131c5cf1.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
