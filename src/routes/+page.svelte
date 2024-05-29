4a6c1eba8029ec3039312ebcaf8ede8b5b15e155
khangtranduc <mpyblock@gmail.com>
2024-05-29 11:07:01 +0700

    [Splash] Animated splash + fleshed out contacts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,10 +7,12 @@
         "projects",
         "blog"
     ]
-    const icons = [
-        "github", "mail", "linkedin", 
-        "instagram", "shield", "twitter"
-    ]
+    const icons = Object.entries({
+        "mdi:github": "https://github.com/khangtranduc", 
+        "mdi:gmail": "mailto:khangductran@duck.com",
+        "mdi:linkedin": "https://www.linkedin.com/in/khangtranduc/", 
+        "ic:baseline-telegram": "https://t.me/khangductran"
+    })
     let showContacts = false;
     let maintain = false;
 </script>
@@ -20,9 +22,9 @@
 <canvas class="glsl-canvas" data-fragment-url="solid.frag" width={innerWidth} height={innerHeight}/>
 
 <div class="ver">
-    <h1 class="hi">HI, <span>i'm <mark>khang</mark></span></h1>
+    <h1>HI, <span>i'm <mark>khang</mark></span></h1>
 
-    <div class="hor">
+    <div class="links">
         {#each links as link, i}
         <h2><a href={"#"} class="contrast">{link}</a></h2>
         {#if i < links.length - 1}
@@ -39,11 +41,12 @@
         on:mouseenter={() => maintain = showContacts}
         on:mouseleave={() => maintain = false}>
         {#if showContacts || maintain}
-        {#each icons as icon, i}
-        <iconify-icon 
-                in:fade|global={{ delay: (icons.length - i) * 100, duration: 300 }} 
-                out:fade|global={{ delay: i * 100, duration: 300 }}
-                width="30" icon="lucide:{icon}"/>
+        {#each icons as [icon, link], i}
+        <a href="{link}" target="_blank"
+            in:fade|global={{ delay: (icons.length - i) * 100, duration: 300 }} 
+            out:fade|global={{ delay: i * 100, duration: 300 }}>
+            <iconify-icon width="40" icon="{icon}"/>
+        </a>
         {/each}
         {/if}
     </div>
@@ -56,6 +59,15 @@
         height: 100vh;
         background: $grey-50;
     }
+    mark {
+        background-color: transparent;
+        animation-name: highlight;
+        animation-duration: 3s;
+        animation-fill-mode: forwards;
+        background-size: 200%;
+        background-image: linear-gradient(to right, $amber-100 50%, transparent 50%),
+            linear-gradient($grey-50, $grey-50);
+    }
     h1 {
         font-size: 3.5rem;
         font-weight: 900;
@@ -63,6 +75,8 @@
             font-size: 3.5rem;
             font-weight: bold;
         }
+        animation-name: intro;
+        animation-duration: 3s;
     }
     h2 {
         color: $grey-600
@@ -90,10 +104,12 @@
             }
         }
     }
-    .hor {
+    .links {
         width: 100%;
         display: flex;
         justify-content: space-between;
+        animation-name: links;
+        animation-duration: 5s;
     }
     .ver {
         display: flex;
@@ -103,5 +119,27 @@
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
+    }
+
+    @keyframes highlight {
+        0% {background-position: 100%;}
+        66% {background-position: 100%;}
+        100% {background-position: 0%;}
+    }
+
+    @keyframes intro {
+        0% {transform: translate(0, 100%);
+            clip-path: polygon(0 110%, 0 110%, 100% 110%, 100% 110%);}
+        17% {transform: translate(0, 100%);
+            clip-path: polygon(0 110%, 0 110%, 100% 110%, 100% 110%);}
+        33% {transform: translate(0, 100%);
+            clip-path: polygon(0 110%, 0 0, 100% 0, 100% 110%);}
+        100% {transform: translate(0, 0);}
+    }
+
+    @keyframes links {
+        0% {opacity: 0;}
+        33% {opacity: 0;}
+        100% {opacity: 1;}
     }
 </style>
